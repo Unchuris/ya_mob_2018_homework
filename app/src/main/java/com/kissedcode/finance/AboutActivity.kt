bbd6a8033c1c7837430e0aaeae22596745e47bda
kissed <kissed.code@yandex.ru>
2018-07-22 22:08:28 +0300

    About screen added

---
+++ app/src/main/java/com/kissedcode/finance/AboutActivity.kt
@@ -0,0 +1,14 @@
+package com.kissedcode.finance
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class AboutActivity : AppCompatActivity() {
+
+    // lifecycle ///////////////////////////////////////////////////////////////////////////////
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about)
+    }
+}
--- app/src/main/java/com/kissedcode/finance/MainActivity.kt
+++ app/src/main/java/com/kissedcode/finance/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.kissedcode.finance
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.ArrayAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -12,6 +14,17 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.main_options, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        when (item?.itemId) {
+            R.id.menuitem_about -> startActivity(
+                    Intent(this, AboutActivity::class.java)
+            )
+        }
+
         return true
     }
 
