8b0a573d9857d4f89054d8cf3bdfb4cc51851b94
kissed <kissed.code@yandex.ru>
2018-07-23 11:04:31 +0300

    Calculator logic added and tested (money operation summarize)

---
+++ app/src/main/java/com/kissedcode/finance/Entities.kt
@@ -0,0 +1,14 @@
+package com.kissedcode.finance
+
+enum class Currency(val cost: Double = 1.0) {
+    USD,
+    RUB(1/60.5)
+}
+
+class MoneyOperation(val type: Type, val value: Double, val currency: Currency = Currency.USD) {
+
+    enum class Type {
+        PLUS,
+        MINUS
+    }
+}
---
+++ app/src/test/java/com/kissedcode/finance/CalculatorTest.kt
@@ -0,0 +1,42 @@
+package com.kissedcode.finance
+
+import com.kissedcode.finance.MoneyOperation.Type.*
+import com.kissedcode.finance.Currency.*
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class CalculatorTest {
+
+    val calculator = Calculator()
+
+    @Test
+    fun `calculator summarize right`() {
+
+        var actual: Double
+        var expected: Double
+        val delta = 0.0
+
+        // 100.55 USD + 50.20 USD - 150.75 USD = 0 USD
+        expected = 0.0
+        actual = calculator.sum(
+                MoneyOperation(PLUS, 100.55, USD),
+                MoneyOperation(PLUS, 50.20, USD),
+                MoneyOperation(MINUS, 150.75, USD)
+        )
+
+        assertEquals(expected, actual, delta)
+
+        // 100 USD + 50 USD - 605 RUB = 140 USD
+        expected = 140.0
+        actual = calculator.sum(
+                MoneyOperation(PLUS, 100.0, USD),
+                MoneyOperation(PLUS, 50.0, USD),
+                MoneyOperation(MINUS, 605.0, RUB)
+        )
+
+        assertEquals(expected, actual, delta)
+
+
+    }
+}
