106dbbc23aac94bfd38a72e55a9c6c59bdf097ae
kissed <kissed.code@yandex.ru>
2018-07-29 22:01:36 +0300

    Currencies repository added with retrofit and rx

---
+++ app/src/main/java/com/kissedcode/finance/model/CurrenciesRepository.kt
@@ -0,0 +1,48 @@
+package com.kissedcode.finance.model
+
+import com.kissedcode.finance.model.dto.CbrResponse
+import com.kissedcode.finance.model.dto.Valute
+import io.reactivex.Observable
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+interface CbrApi {
+    @GET("daily_json.js")
+    fun getCurrencies(): Observable<CbrResponse>
+}
+
+object RemoteRepository {
+
+    val cbrApi by lazy {
+        val retrofit = Retrofit.Builder()
+                .baseUrl("https://www.cbr-xml-daily.ru/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build()
+
+        val api = retrofit.create(CbrApi::class.java)
+        api
+    }
+
+    // interface ///////////////////////////////////////////////////////////////////////////////
+
+    fun getValute() = cbrApi.getCurrencies().map { it.valute }
+}
+
+object CacheRepository {
+
+    val remote = RemoteRepository
+
+    var cache: Valute? = null
+
+    // interface ///////////////////////////////////////////////////////////////////////////////
+
+    fun getValute(): Observable<Valute?> {
+        if (cache != null)
+            return Observable.just(cache)
+        else
+            return remote.getValute().doAfterNext { cache = it }
+    }
+}
